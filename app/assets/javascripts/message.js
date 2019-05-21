e27fd10d3080739efcf36f4785bd4f46fd6f478c
atsuto <g.atsuto@icloud.com>
2019-05-21 17:39:50 +0900

    非同期通信機能実装

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,57 @@
+$(function(){
+  function buildHTML(message){
+    var img = ""
+    if (message.image.url !== null) {
+        img = `<img src="${message.image.url}">`
+    }
+    var html = ` 
+               <div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                <div class="lower-meesage">
+                <p class="lower-message__content">
+                   ${message.content}
+                </p>
+                ${img}
+                </div>
+                </div>`
+                
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    console.log(this)
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+  })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.ajax-message').append(html)
+    $('.form__message').val('')
+    $('.hidden').val('')
+    $('.form__submit').prop('disabled', false); 
+  })
+  .fail(function(){
+    alert('error');
+    $('.form__submit').prop('disabled', false); 
+  })
+  $('.messages').animate({
+    scrollTop: $(document).height()
+  })
+
+})
+
+})
