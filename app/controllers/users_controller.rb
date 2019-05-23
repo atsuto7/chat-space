224997ca1361b6c53a343a10a72752b6fc95b43c
atsuto <g.atsuto@icloud.com>
2019-05-23 12:15:19 +0900

    Update users_controller.rb
    limitを削除しました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def search
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20).where.not(id: current_user.id)
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
     respond_to do |format|
       format.html
       format.json
