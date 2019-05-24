60e35017426455b9a483df09622c1bb2e1d4a1e7
atsuto <g.atsuto@icloud.com>
2019-05-24 19:38:07 +0900

    Update messages_controller.rb

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,5 @@
 class Api::MessagesController < ApplicationController
   def index
-    message = Message.find(params[:id])
     group_messages = Message.where(params[:group_id])
     @messages = group_messages.where('id > ?', params[:id])
     respond_to do |format|
