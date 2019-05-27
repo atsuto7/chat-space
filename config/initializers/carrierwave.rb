bf06991d1c2c46ebc18a118893731cbb97e99618
atsuto <g.atsuto@icloud.com>
2019-05-27 15:07:18 +0900

    綴りミス修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1' #例 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'caht-space-atsuto7'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-atsuto7'
+  config.fog_directory  = 'chat-space-atsuto7'
+  config.asset_host = 'https://s3-ap-northeast-1.am azonaws.com/chat-space-atsuto7'
 end
