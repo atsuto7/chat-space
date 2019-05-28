9cda4811ac39702f2b28d430de6b3f992be67e44
atsuto <g.atsuto@icloud.com>
2019-05-28 16:22:54 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,17 +7,15 @@ class MessagesController < ApplicationController
   
   def create
     @message = @group.messages.new(message_params)
-
-        if @message.save
+  if @message.save
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
-      respond_to do |format|
-
-        format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'  }
-        format.json
-      end
+  end
+  respond_to do |format|
+    format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'  }
+    format.json
   end
 end
   private
